24eb6c51c206804419fc87548217612729c26368
dj.moon <djmoon@biplatform.co.kr>
2025-04-10 09:49:07 +0900

    최초생성

---
+++ src/main/java/com/moneport/backend/MoneportApplication.java
@@ -0,0 +1,13 @@
+package com.moneport.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moneport/backend/MoneportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneport.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
